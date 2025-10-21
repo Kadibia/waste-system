ab4acf42a833161dc7779b1d30f4461b624d50c3
Kadibia <Ikevudechukwuka@gmail.com>
2025-10-21 13:12:40 +0100

    requirements created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name =  'wasteSystem',
+    version =  '0.0.0',
+    author = 'Chukwuka Ikevude',
+    author_email =  'ikevudechukwuka@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
